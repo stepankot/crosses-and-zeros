52846f730965225eddbfdf03920a5afe6bfa251b
STAP16 <stepancat61@gmail.com>
2025-12-29 11:57:54 +0300

    fix checkGame logic

--- src/Field/Field.jsx
+++ src/Field/Field.jsx
@@ -1,48 +1,47 @@
-import { checkGame } from "../utils/checkGame";
-import FieldLayout from "./FieldLayout";
+import { checkGame } from '../utils/checkGame'
+import FieldLayout from './FieldLayout'
 
 export default function Field({ ...props }) {
-  const {
-    field,
-    setField,
-    setIsDraw,
-    setIsGameOver,
-    setWinner,
-    currentPlayer,
-    setCurrentPlayer,
-    isGameOver,
-  } = props;
+	const {
+		field,
+		setField,
+		setIsDraw,
+		setIsGameOver,
+		setWinner,
+		currentPlayer,
+		setCurrentPlayer,
+		isGameOver
+	} = props
 
-  const checkWinnerX = checkGame(field, "X");
-  const checkWinnerO = checkGame(field, "O");
-  if (checkWinnerX === "X") {
-    setWinner("X");
-    setIsGameOver(true);
-  }
-  if (checkWinnerO === "O") {
-    setWinner("O");
-    setIsGameOver(true);
-  }
-  if (checkWinnerO === "Draw" || checkWinnerX === "Draw") {
-    setIsDraw(true);
-    setIsGameOver(true);
-  }
+	const onGameCheck = index => {
+		if (isGameOver) return
+		if (field[index] !== '') return
 
-  const changePlayer = (player) => {
-    if (player === "X") {
-      setCurrentPlayer("O");
-    } else {
-      setCurrentPlayer("X");
-    }
-  };
+		const newField = [...field]
+		newField[index] = currentPlayer
+		setField(newField)
 
-  return (
-    <FieldLayout
-      field={field}
-      setField={setField}
-      currentPlayer={currentPlayer}
-      changePlayer={changePlayer}
-      isGameOver={isGameOver}
-    ></FieldLayout>
-  );
+		const result = checkGame(newField, currentPlayer)
+
+		if (result === 'X' || result === 'O') {
+			setWinner(result)
+			setIsGameOver(true)
+			return
+		}
+
+		if (result === 'Draw') {
+			setIsDraw(true)
+			setIsGameOver(true)
+			return
+		}
+
+		setCurrentPlayer(prev => (prev === 'X' ? 'O' : 'X'))
+	}
+
+	return (
+		<FieldLayout
+			field={field}
+			onClick={onGameCheck}
+		></FieldLayout>
+	)
 }
--- src/Field/FieldLayout.jsx
+++ src/Field/FieldLayout.jsx
@@ -1,32 +1,15 @@
-export default function FieldLayout({
-  field,
-  setField,
-  currentPlayer,
-  changePlayer,
-  isGameOver,
-}) {
-  return (
-    <div className="gameField">
-      {field.map((fieldBox, index) => (
-        <div
-          className="fiedlValueBox"
-          key={index}
-          onClick={() => {
-            const newField = [...field];
-            if (newField[index] !== "") {
-              return;
-            }
-            if (isGameOver) {
-              return;
-            }
-            newField[index] = currentPlayer;
-            setField(newField);
-            changePlayer(currentPlayer);
-          }}
-        >
-          {fieldBox}
-        </div>
-      ))}
-    </div>
-  );
+export default function FieldLayout({ field, onClick }) {
+	return (
+		<div className="gameField">
+			{field.map((fieldBox, index) => (
+				<div
+					className="fiedlValueBox"
+					key={index}
+					onClick={() => onClick(index)}
+				>
+					{fieldBox}
+				</div>
+			))}
+		</div>
+	)
 }
